4d8924b2fe017930b9ad561a54461c4c3f3c0f17
김종혁 <notify9637@naver.com>
2023-08-09 17:07:47 +0900

    prob04 완료

--- practice03/src/main/java/prob04/StringUtil.java
+++ practice03/src/main/java/prob04/StringUtil.java
@@ -1,4 +1,11 @@
 package prob04;
 
 public class StringUtil {
+	public static String concatenate(String[] args){
+		StringBuilder result = new StringBuilder();
+        for (String s : args) {
+            result.append(s);
+        }
+        return result.toString();
+	}
 }
