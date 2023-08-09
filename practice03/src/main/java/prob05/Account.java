a291e237cb9cea12f9fae69af16c5edbceb7f48c
김종혁 <notify9637@naver.com>
2023-08-09 17:18:35 +0900

    prob05 완료

--- practice03/src/main/java/prob05/Account.java
+++ practice03/src/main/java/prob05/Account.java
@@ -2,7 +2,28 @@ package prob05;
 
 public class Account {
 	private String accountNo;
+	private int balance;
 	public Account(String accountNo) {
 		this.accountNo = accountNo;
+		System.out.println(accountNo + "계좌가 개설되었습니다.");
+		System.out.println(accountNo + "계좌의 잔고는 " + balance + "만원입니다.");
+		
 	}
+	
+	public String getAccountNo() {
+		return accountNo;
+	}
+	public int getBalance() {
+		return balance;
+	}
+	
+	public void save(int amount) {
+		balance += amount;
+		System.out.println(accountNo + " 계좌에 " + amount + "만원이 입금되었습니다.");
+	}
+	public void deposit(int amount) {
+		balance -= amount;
+	}
+	
+	
 }
