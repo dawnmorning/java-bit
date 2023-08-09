9050e6729894d4e5f2f9a636517110a433bf4836
김종혁 <notify9637@naver.com>
2023-08-09 17:02:55 +0900

    prob03 완료

---
+++ practice03/src/main/java/prob03/CurrencyConverter.java
@@ -0,0 +1,16 @@
+package prob03;
+
+public class CurrencyConverter {
+	private static double rate;
+	
+	public static void setRate(double r) {
+		rate = r;
+	}
+	public static double toDollar(double won) {
+		return won / rate;
+	}
+	
+	public static double toKRW(double dollar) {
+		return dollar * rate;
+	}
+}
