4ada2b8f4aafeac5a1f9456b9ddf243c0e0f0319
김종혁 <notify9637@naver.com>
2023-08-09 16:53:45 +0900

    practice03 prob02 번 완료

--- practice03/src/main/java/prob02/GoodsApp.java
+++ practice03/src/main/java/prob02/GoodsApp.java
@@ -11,14 +11,35 @@ public class GoodsApp {
 		Goods[] goods = new Goods[COUNT_GOODS];
 
 		// 상품 입력
-		String line = scanner.nextLine();
-		String[] infos = line.split(" ");
-		String name= infos[0];
-		int price = Integer.parseInt(infos[1]);
-		int count = Integer.parseInt(infos[2]);
-		// 상품 출
-		
+		for (int i = 0; i < COUNT_GOODS; i++) {
+			String line = scanner.nextLine();
+			String[] infos = line.split(" ");
+			String name= infos[0];
+			int price = Integer.parseInt(infos[1]);
+			int count = Integer.parseInt(infos[2]);
+			goods[i] = new Goods(name, price, count);
+		}
+		// 상품 출고
+//		for (Goods good : goods) {
+//			System.out.println(good.name + "(가격: " + good.price + "원)이 " + + good.quantity + 
+//					"개 입고 되었습니다.");
+//		}
+		// 상품 출고
+				for (int i = 0; i < COUNT_GOODS; i++) {
+					System.out.println(names[i] + "(가격: " + prices[i] + "원)이 " + quantities[i] + "개 입고 되었습니다.");
+				}
 		// 자원정리
 		scanner.close();
 	}
+//	static class Goods{
+//		String name;
+//		int price;
+//		int count;
+//		
+//		public Goods(String name, int price, int count) {
+//			this.name = name;
+//			this.price = price;
+//			this.count = count;
+//		}
+//	}
 }
