ea10b076887fb03a67078197e85e680f4e0e3467
김종혁 <notify9637@naver.com>
2023-08-10 16:35:58 +0900

    practice04 prob01 완료

--- practice04/src/main/java/prob01/PrinterTest.java
+++ practice04/src/main/java/prob01/PrinterTest.java
@@ -1,13 +1,35 @@
 package prob01;
 
-public class PrinterTest {
+import java.security.PublicKey;
 
+public class PrinterTest {
+	
 	public static void main(String[] args) {
 		Printer printer = new Printer();
-
+		
 		printer.println( 10 );
 		printer.println( true );
 		printer.println( 5.7 );
 		printer.println( "홍길동" );
 	}
+	static class Printer{
+		public void println(String str) {
+			System.out.println(str);
+		}
+		public void println(int number) {
+			System.out.println(number);
+		}
+		public void println(boolean bool) {
+			System.out.println(bool);
+		}
+		public void println(double number) {
+			System.out.println(number);
+		}
+	}
 }
+
+// Printer 클래스가 PrinterTest 클래스의 내부 클래스로 선언되어 있기 때문에 이러한 오류가 발생합니다.
+
+// 내부 클래스의 인스턴스를 생성하기 위해서는 외부 클래스의 인스턴스가 필요합니다. 
+//이를 해결하기 위해 Printer 클래스를 static으로 선언하면 됩니다. 
+// static 내부 클래스는 외부 클래스의 인스턴스 없이도 인스턴스화 할 수 있습니다.
