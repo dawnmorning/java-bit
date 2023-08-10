11d134b4464bae3bb066ac00c98ec36c874b7393
김종혁 <notify9637@naver.com>
2023-08-10 17:03:17 +0900

    practice04 prob03 완료

--- practice04/src/main/java/prob03/Tank.java
+++ practice04/src/main/java/prob03/Tank.java
@@ -1,15 +1,14 @@
 package prob03;
 
-public class Tank{
+public class Tank extends Unit{
 	// 현재 위치
-	private int x;
-	private int y;
 
-	void move(int x, int y) {
+	@Override
+	public void move(int x, int y) {
 		/* 지정된 위치로 이동 */
 	}
-
-	void stop() {
+	@Override
+	public void stop() {
 		/* 현재 위치에 정지 */
 	}
 	
---
+++ practice04/src/main/java/prob03/Unit.java
@@ -0,0 +1,15 @@
+package prob03;
+
+public class Unit {
+	protected int x;
+	protected int y;
+	
+	public void move(int x, int y) {
+		
+	}
+	
+	public void stop() {
+		
+	}
+	
+}
