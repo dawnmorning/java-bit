1a3d546494ed1400c5a2f0eafa33e287b046c19b
김종혁 <notify9637@naver.com>
2023-08-11 17:47:02 +0900

    practice05 prob06 완료

---
+++ practice05/src/main/java/prob6/Rectangle.java
@@ -0,0 +1,30 @@
+package prob6;
+
+public class Rectangle extends Shape implements Resizable{
+	private double width;
+	private double height;
+
+	public Rectangle(double width, double height) {
+		this.width = width;
+		this.height = height;
+	}
+
+	@Override
+	public double getArea() {
+		return width * height;
+
+	}
+
+	@Override
+	public double getPerimeter() {
+		return 2 * width + 2 * height;
+
+	}
+
+	@Override
+	public void resize(double ratio) {
+		width *= ratio;
+		height *= ratio;
+	}
+
+}
