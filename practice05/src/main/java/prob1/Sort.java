4c9863e74061bff76a49933bdad89c09224bf980
김종혁 <notify9637@naver.com>
2023-08-11 16:57:23 +0900

    practice05 prob01 완료

---
+++ practice05/src/main/java/prob1/Sort.java
@@ -0,0 +1,36 @@
+package prob1;
+
+public class Sort {
+	
+	public static void main(String[] arg) {
+	
+		int array[] = { 5, 9, 3, 8, 60, 20, 1 };
+		int count =  array.length;
+		
+		System.out.println( "Before sort." );
+		
+		for (int i = 1; i < count; i++) {
+			System.out.print( array[ i ] + " " );
+		}
+		
+		//
+		for (int i = count - 1; i > 0; i--) {
+			for (int j = 0; j < i; j++) {
+				if(array[j] > array[j+1]) {
+					int temp = array[j];
+					array[j] = array[j+1];
+					array[j+1] = temp;
+				}
+			}
+		}
+		//
+
+		
+		// 결과 출력
+		System.out.println( "\nAfter Sort." );
+		
+		for (int i = 0; i < count; i++) {
+			System.out.print(array[i] + " ");
+		}		
+	}
+}
