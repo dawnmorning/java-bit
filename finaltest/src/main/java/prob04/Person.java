6c8416e5ca79bbd25bd49f38a2a16c41fc9d325e
김종혁 <notify9637@naver.com>
2023-08-16 16:39:50 +0900

    final test init

---
+++ finaltest/src/main/java/prob04/Person.java
@@ -0,0 +1,14 @@
+package prob04;
+
+public class Person {
+	private static int numberOfPerson; // 전체 인구수
+	private int age;
+	private String name;
+	
+	/* 코드 작성 */
+	public Person(int age, String name) {
+		this.age = age;
+		this.name = name;
+				
+	}
+}
