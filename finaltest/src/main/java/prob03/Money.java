5c0ea0c95bfc31c84ee8bfc28903ab9dadaf9906
김종혁 <notify9637@naver.com>
2023-08-16 17:02:17 +0900

    finaltest 03 완료

--- finaltest/src/main/java/prob03/Money.java
+++ finaltest/src/main/java/prob03/Money.java
@@ -4,4 +4,27 @@ public class Money {
 	private int amount;
 	
 	/* 코드 작성 */
+	public Money(int amount) {
+		this.amount = amount;
+	}
+	public Money add(Money amounts) {
+		return new Money(this.amount + amounts.amount );
+	}
+	public Money minus(Money amounts) {
+		return new Money(this.amount - amounts.amount );
+	}
+	public Money multiply(Money amounts) {
+		return new Money(this.amount * amounts.amount );
+	}
+	public Money devide(Money amounts) {
+		return new Money(this.amount / amounts.amount );
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Money) {
+			Money amounts = (Money) obj;
+			return this.amount == amounts.amount;
+		}
+		return false;
+	}
 }
